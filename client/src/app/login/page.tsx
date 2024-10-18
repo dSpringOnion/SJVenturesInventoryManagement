bbd99df480f7457e4420b3d7c2223c27781afe79
Daniel Park <dpa2@iu.edu>
2024-10-18 13:25:08 -0400

    updated login page

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -17,7 +17,7 @@ const Login = () => {
         setError("");
 
         try {
-            const response = await fetch("/api/auth/login", {
+            const response = await fetch("http://18.191.222.14/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
