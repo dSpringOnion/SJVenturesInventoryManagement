65089ae7964abb08834ceb98ccb26cdfe9e8f4a8
Daniel Park <dpa2@iu.edu>
2024-10-31 21:49:34 -0400

    changed dashboard

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,11 +4,24 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://sj-inv-managment.s3.us-east-2.amazonaws.com/",
+        hostname: "sj-inv-managment.s3.us-east-2.amazonaws.com",
         port: "",
         pathname: "/**",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://sj-inv-managment.s3.us-east-2.amazonaws.com; connect-src 'self';",
+          },
+        ],
+      },
+    ];
   },
 };
 
